7884aff0abdd42f83b2d378865b8149aaba4ebda
JeremyLoh <jeremyloh03@gmail.com>
2024-06-17 00:42:28 +0800

    feat: add html-webpack-plugin package and webpack config

---
+++ restaurant/webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant",
+      // Default is lodash template function https://lodash.com/docs/4.17.15#template
+      template: "src/index.html",
+    }),
+  ],
+}

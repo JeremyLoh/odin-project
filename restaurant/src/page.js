8e232199ae5ad47d463f7bcd8c18c29e932b5f80
JeremyLoh <jeremyloh03@gmail.com>
2024-06-17 19:05:54 +0800

    feat: add restaurant location and hours

--- restaurant/src/page.js
+++ restaurant/src/page.js
@@ -2,7 +2,7 @@ import HeroImg from "./assets/images/hero.jpeg"
 import BackgroundImg from "./assets/images/background.jpeg"
 
 export function setup(contentContainer) {
-  contentContainer.append(setupHero())
+  contentContainer.append(setupHero(), setupLocation(), setupHours())
 }
 
 function setupHero() {
@@ -19,4 +19,36 @@ function setupHero() {
   heroImg.classList.add("hero-img")
   container.append(header, h2, heroImg)
   return container
+}
+
+function setupLocation() {
+  const card = document.createElement("div")
+  card.classList.add("card")
+  const cardTitle = document.createElement("p")
+  cardTitle.classList.add("card-title")
+  cardTitle.textContent = "Location"
+  const location = document.createElement("p")
+  location.textContent = "123 Coffee Street, Singapore 123456"
+  card.append(cardTitle, location)
+  return card
+}
+
+function setupHours() {
+  const card = document.createElement("div")
+  card.classList.add("card")
+  const cardTitle = document.createElement("p")
+  cardTitle.classList.add("card-title")
+  cardTitle.textContent = "Hours"
+  const ul = document.createElement("ul")
+  const itemText = ["Monday to Friday: 7am - 5pm",
+    "Saturday: 8am - 6pm",
+    "Sunday: 8am - 4pm"]
+  const itemTextElements = itemText.map(text => {
+    const li = document.createElement("li")
+    li.append(document.createTextNode(text))
+    return li
+  })
+  ul.append(...itemTextElements)
+  card.append(cardTitle, ul)
+  return card
 }

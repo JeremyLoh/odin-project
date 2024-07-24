cf2aee78bed9c38a3bc9416ac3479da83713a261
JeremyLoh <jeremyloh03@gmail.com>
2024-07-24 23:41:14 +0800

    feat: add more test for analyzeArray

--- testing-practice/analyzeArray.test.js
+++ testing-practice/analyzeArray.test.js
@@ -36,5 +36,11 @@ describe("analyzeArray", () => {
       max: 4,
       length: 3,
     })
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    })
   })
 })
